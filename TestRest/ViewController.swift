834f7b880720b98815f49a77f69aed036a5d1767
Leonaire <fhg>
2020-01-22 09:59:40 +0100

    Initial Commit

---
+++ TestRest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestRest
+//
+//  Created by mobapp07 on 22/01/2020.
+//  Copyright © 2020 mobapp07. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
